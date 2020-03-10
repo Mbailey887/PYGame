52ee832cc88a27287558398a9dd52cb40b07c1a5
Bailey <Michael_Bailey2@baylor.edu>
2020-03-10 15:22:52 -0500

    shifted bullet code to update bullets method

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -31,14 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
             
             
@@ -74,6 +67,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        #update bullet positions.
+        self.bullets.update()
+
+        #Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
