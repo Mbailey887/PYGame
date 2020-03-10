9db5b19f278d79ca490bc94abf558707ca6cdf1c
Bailey <Michael_Bailey2@baylor.edu>
2020-03-09 23:38:12 -0500

    refactored for ship movement. added quit option.

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen #assign the screen to an attribute of Ship, so we can access it easily in all methods in this class
+        self.settings. = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect() #acess the screen's rect attribute. allows us to place the ship in the correct location on the screen  
         
         # load the ship image and get its rect.
@@ -14,6 +15,25 @@ class Ship:
 
         # Start with each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """ Update the ship's position based on the movement flag.""""
+        # update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x
+        self.rect.x = self.x
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
